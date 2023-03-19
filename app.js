1c072c2b46f215fcaebc4fa3d9b2144b0fb6a6c7
ela_lisondra <emrlisondra@gmail.com>
2023-03-19 18:17:38 +0000

    connect to database

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose  = require("mongoose");
+
+// const date = require(__dirname + "/date.js");
 
 const app = express();
-
-const items = [];
-const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('CONNECTED TO MONGO!');
+    })
+    .catch((err) => {
+        console.log('ERROR');
+        console.log(err);
+    });
+
+const itemsSchema = new mongoose.Schema ({
+    name: String
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item ({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item ({
+    name: "Hit + button to add a new item."
+});
+
+const item3 = new Item ({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems)
+    .then(function (){
+        console.log("Successfully added default items!");
+    })
+    .catch((err) => {
+        console.log('ERROR');
+        console.log(err);
+    });
+
+
 
 app.get("/", function(req, res){
 
@@ -26,7 +65,7 @@ app.get("/", function(req, res){
     // today day = today.toLocaleDateString("en-US", options);
 
    
-const day = date.getDate();   
+// const day = date.getDate();   
 
     res.render("list", {listTitle: day, newListItems: items});
 })
